d1c76214bc6e2044a01bf60f0bd7e7d744da4197
N-Angleton <nick.angleton@gmail.com>
2022-04-26 15:18:42 -0400

    add demo user

--- frontend/components/login/login.jsx
+++ frontend/components/login/login.jsx
@@ -12,12 +12,32 @@ export class Login extends React.Component{
     }
 
     handleSubmit(e){
-        e.preventDefault()
+        if (e) e.preventDefault()
         this.props.processForm({ username: this.state.username, password: this.state.password})
     }
 
     update(e){
         this.setState( { [e.target.name]: e.target.value } )
+    }
+
+    async loginDemo(e){
+        this.setState( {username: "", password: ""} )
+        let user = "Demo_User"
+        let pass = "Demo_Password"
+        for (let i = 0; i < user.length; i++) {
+            await this.timeout(150)
+            this.setState( {username: (this.state.username + user[i] )})
+        }
+        for (let i = 0; i < pass.length; i++) {
+            await this.timeout(150)
+            this.setState( {password: (this.state.password + pass[i] )})
+        }
+        await this.timeout(250)
+        this.handleSubmit()
+    }
+
+    timeout(ms){
+        return new Promise( resolve => setTimeout(resolve, ms));
     }
 
     render(){
@@ -27,16 +47,21 @@ export class Login extends React.Component{
             return (
                 <div className="signinContainer">
 
-                    <form onSubmit={this.handleSubmit} className="signinForm">
-                        <h1 className="signinHeader">Sign In</h1>
-                        <label>Username
-                            <input type="text" name="username" onChange={this.update} className="signinUsername"/>
-                        </label>
-                        <label>Password
-                            <input type="text" name="password" onChange={this.update} className="signinPassword"/>
-                        </label>
-                        <button type="submit" className="signinButton">Sign In</button>
-                    </form>
+                    <div>
+                        <form onSubmit={this.handleSubmit} className="signinForm">
+                            <h1 className="signinTitle">Sign In</h1>
+                            <label>Username
+                                <input type="text" value={this.state.username} name="username" onChange={this.update} className="signinUsername"/>
+                            </label>
+                            <label>Password
+                                <input type="text" value={this.state.password} name="password" onChange={this.update} className="signinPassword"/>
+                            </label>
+                            <button type="submit" className="signinButton">Sign In</button>
+                        </form>
+                        <h2 className="or">OR</h2>
+                        <button className="demoUserButton" onClick={() => this.loginDemo()}>Sign in as demo user</button>
+                    </div>
+                        
 
                     <div className="signupLinkContainer">
                         <Link to="/signup" className="createAccountLink">Create an account</Link>
