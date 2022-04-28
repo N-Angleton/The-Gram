8d11266122ff6bd5ae869a64c36d21e70944b62b
N-Angleton <nick.angleton@gmail.com>
2022-04-28 13:48:38 -0400

    improve css

--- frontend/components/login/login.jsx
+++ frontend/components/login/login.jsx
@@ -9,6 +9,8 @@ export class Login extends React.Component{
         this.state = { username: "", password: ""}
         this.handleSubmit = this.handleSubmit.bind(this)
         this.update = this.update.bind(this)
+        this.hidden = true
+        this.flipHidden = this.flipHidden.bind(this)
     }
 
     handleSubmit(e){
@@ -24,15 +26,19 @@ export class Login extends React.Component{
         this.setState( {username: "", password: ""} )
         let user = "Demo_User"
         let pass = "Demo_Password"
+        await this.timeout(125)
+        document.getElementById('signInUsernameInput').focus();
         for (let i = 0; i < user.length; i++) {
             await this.timeout(150)
             this.setState( {username: (this.state.username + user[i] )})
         }
+        await this.timeout(175)
+        document.getElementById('signInPasswordInput').focus();
         for (let i = 0; i < pass.length; i++) {
-            await this.timeout(150)
+            await this.timeout(125)
             this.setState( {password: (this.state.password + pass[i] )})
         }
-        await this.timeout(250)
+        await this.timeout(100)
         this.handleSubmit()
     }
 
@@ -40,17 +46,12 @@ export class Login extends React.Component{
         return new Promise( resolve => setTimeout(resolve, ms));
     }
 
-    // retrieveUsernameInput(){
-    //     if (this.state.username.length === 0) {
-    //         return <input type="text" className="signInUsernameInput" value={this.state.username} name="username" onChange={this.update} />
-    //     } else {
-    //         return <input type="text" className="signInUsernameInput" value={this.state.username} name="username" onChange={this.update} />
-    //     }
-    // }
+    flipHidden(e){
+        e.preventDefault()
+        this.hidden = !this.hidden
+        this.forceUpdate()
+    }
 
-    // retrievePasswordInput(){
-
-    // }
 
     render(){
         if (this.props.session !== null){
@@ -60,6 +61,10 @@ export class Login extends React.Component{
             if (this.state.username.length) usernameContainerClassName += " transition"
             let passwordContainerClassName = "signInPasswordInputContainer"
             if (this.state.password.length) passwordContainerClassName += " transition"
+            let disabled = true
+            if (this.state.username.length && this.state.password.length > 5) {
+                disabled = false;
+            }
             return (
                 <div className="signInGrid">
 
@@ -68,14 +73,16 @@ export class Login extends React.Component{
 
                         <div className="signInContainer">
                             <form onSubmit={this.handleSubmit} className="signInForm">
-                                <h1 className="signInTitle">The Gram</h1>
+                                <h1 className="signInTitleWithImg">the <img src={window.logoURL} alt="gram" /></h1>
+                                <h1 className="signInTitle">the gram</h1>
                                     <div className={usernameContainerClassName}>
-                                        <input type="text" className="signInUsernameInput" value={this.state.username} name="username" onChange={this.update} required />
+                                        <input type="text" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" id="signInUsernameInput" className="signInUsernameInput" value={this.state.username} name="username" onChange={this.update} />
                                     </div>
-                                    <div className="signInPasswordInputContainer">
-                                        <input type="text" className="signInPasswordInput" value={this.state.password} name="password" onChange={this.update} required />
+                                    <div className={passwordContainerClassName}>
+                                        <input type={this.hidden ? "password" : "text"} autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" id="signInPasswordInput" className="signInPasswordInput" value={this.state.password} name="password" onChange={this.update} />
+                                        <button className="showHideButton" onClick={(e) => this.flipHidden(e)}>{this.hidden ? "Show" : "Hide"}</button>
                                     </div>
-                                <button type="submit" className="signInButton">Sign In</button>
+                                <button type="submit" disabled={disabled} className="signInButton">Sign In</button>
                             </form>
                             <div className="orContainer">
                                 <div className="line"></div>
