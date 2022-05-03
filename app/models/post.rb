3d9754f5437906e4585eb0ce72c7f1a345fdaa1f
Nicholas Angleton <nick.angleton@gmail.com>
2022-05-03 17:23:59 -0400

    work on aws

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
 
     validates :description, presence: true
 
+    has_one_attched :photo
+
     belongs_to :poster,
         foreign_key: :poster_id,
         class_name: :User
