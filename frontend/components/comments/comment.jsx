8686c7b92bddf4f97feb9b0852368bea832ab8a0
Nicholas <nick.angleton@gmail.com>
2022-09-01 03:51:51 -0400

    edit delete comment

--- frontend/components/comments/comment.jsx
+++ frontend/components/comments/comment.jsx
@@ -43,14 +43,15 @@ export class Comment extends React.Component {
         <Link to={{pathname: `/users/${this.props.commenter.username}`}} className="userProfileLink">
           <span className="commenterUsername">{this.props.commenter.username}</span>
         </Link>
-        {this.props.comment.body}</p>
+        {this.props.comment.body}
         {this.props.commenter.id === this.props.session_id ? (
           <>
             <button className="btn-2" onClick={(e) => this.deleteComment(e)}>delete</button>
           </>
         ) : (
-          <FollowInterfaceContainer otherUser={this.props.commenter.id} />
-        )}
+          // <FollowInterfaceContainer otherUser={this.props.commenter.id} />
+          <></>
+        )}</p>
       </li>
     );
   }
